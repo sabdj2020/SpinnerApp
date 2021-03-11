520b23ff72f216eadf6ecb4307484f2d2ea485ad
msalawu <61276349+msalawu@users.noreply.github.com>
2021-03-11 17:38:53 -0500

    clean

--- Spinner/src/app/app.module.ts
+++ Spinner/src/app/app.module.ts
@@ -7,17 +7,11 @@ import { HomeComponent } from './home/home.component';
 import { SearchItemComponent } from './search-item/search-item.component';
 import { SearchComponent } from './search/search.component';
 import { RegisterComponent } from './register/register.component';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { LoginComponent } from './login/login.component';
-=======
-=======
 import { BandPostComponent } from './band-post/band-post.component';
 import { FormsModule } from '@angular/forms';
->>>>>>> 1dcf0be1eed7f58418e975f72fcc63135e9e5e93
 import { BandComponent } from './band/band.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
->>>>>>> ef4ec7201b3fcd4e05981d231ed94519a7cac386
 
 @NgModule({
   declarations: [
@@ -26,15 +20,9 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     SearchItemComponent,
     SearchComponent,
     RegisterComponent,
-<<<<<<< HEAD
-<<<<<<< HEAD
     LoginComponent
-=======
-=======
     BandPostComponent
->>>>>>> 1dcf0be1eed7f58418e975f72fcc63135e9e5e93
     BandComponent
->>>>>>> ef4ec7201b3fcd4e05981d231ed94519a7cac386
   ],
   imports: [
     BrowserModule,

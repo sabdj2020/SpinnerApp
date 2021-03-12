2b38ee17833406e7dfa481af5a4a86c724dfb55e
msalawu <61276349+msalawu@users.noreply.github.com>
2021-03-12 11:30:13 -0500

    uplod function

--- Spinner/src/app/file-upload/file-upload.service.ts
+++ Spinner/src/app/file-upload/file-upload.service.ts
@@ -7,12 +7,12 @@ import { Observable } from 'rxjs';
 })
 export class FileUploadService {
 
-  baseApiUrl= "ourApiURL"
+  baseUrl= "ourURL"
   constructor(private http:HttpClient) { }
 
   upload(file):Observable<any>{
     const formData=new FormData(); //creating our form data
     formData.append("file", file, file.name); //stores whatever file user uploads
-    return this.http.post(this.baseApiUrl, formData) //this will http post our formData request
+    return this.http.post(this.baseUrl, formData) //this will http post our formData request
   }
 }

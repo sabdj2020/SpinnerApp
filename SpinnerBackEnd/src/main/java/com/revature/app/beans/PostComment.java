b86a53be74d7709f0a1a61b8ef296466be4223d9
Kurt Werber <kurtwerber@Kurts-MBP-2.fios-router.home>
2021-03-15 17:09:52 -0400

    removed transient tags in java beans

--- SpinnerBackEnd/src/main/java/com/revature/app/beans/PostComment.java
+++ SpinnerBackEnd/src/main/java/com/revature/app/beans/PostComment.java
@@ -14,7 +14,6 @@ public class PostComment {
 	private String comment;
 	@Column(name="comment_time")
 	private LocalDateTime commentTime;
-	@Transient
 	private Integer likes;
 	
 	public Integer getId() {

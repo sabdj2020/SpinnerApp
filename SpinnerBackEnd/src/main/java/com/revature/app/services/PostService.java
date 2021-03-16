c4f02e4c942b428033b8b8f7ed3780c48847b802
Kurt Werber <kurtwerber@Kurts-MBP-2.fios-router.home>
2021-03-16 11:04:39 -0400

    comment service layer

--- SpinnerBackEnd/src/main/java/com/revature/app/services/PostService.java
+++ SpinnerBackEnd/src/main/java/com/revature/app/services/PostService.java
@@ -5,6 +5,7 @@ import java.util.Set;
 import com.revature.app.beans.Band;
 import com.revature.app.beans.Music;
 import com.revature.app.beans.Post;
+import com.revature.app.beans.PostComment;
 import com.revature.app.beans.User;
 
 public interface PostService {
@@ -13,4 +14,10 @@ public interface PostService {
 	public Set<Post> getPostsByBand(Band b);
 	public Set<Post> getPostsBySong(Music m);
 	public void deletePost(Post p);
+	
+	public Integer addComment(PostComment comment, Post p, User u);
+	public Set<PostComment> getCommentsByUser(User u);
+	public Set<PostComment> getCommentsByPost(Post p);
+	public void updateComment(PostComment comment);
+	public void deleteComment(PostComment comment);
 }

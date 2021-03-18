c622af44b4ec5fa91e918839ba10c64e6e4e1e82
Ryan Lansdon <ryanlansdon@gmail.com>
2021-03-17 21:11:17 -0600

    Added routing and major styling changes

---
+++ Spinner/src/app/band-view/band-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-band-view',
+  templateUrl: './band-view.component.html',
+  styleUrls: ['./band-view.component.css']
+})
+export class BandViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

ae723628c393f4ab40199856206f7a8dda7467a0
Ryan Lansdon <ryanlansdon@gmail.com>
2021-03-19 14:27:06 -0600

    Added search by song

--- Spinner/src/app/navbar/navbar.component.ts
+++ Spinner/src/app/navbar/navbar.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchService } from '../services/search.service';
 import { UserService } from '../services/user.service';
+import { Track } from '../models/result-model';
+import { PostService } from '../services/post.service';
+import { Post } from '../models/post';
 
 @Component({
   selector: 'app-navbar',
@@ -7,10 +11,33 @@ import { UserService } from '../services/user.service';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  tracks: Track[] = [];
+  searchTrack: Track;
+  searchPosts: Post[] = [];
 
-  constructor(public userServ: UserService) { }
+  constructor(public userServ: UserService, public searchServ: SearchService, public postServ: PostService) { }
 
   ngOnInit(): void {
   }
 
+  search(term: string) {
+    if (!term || term == "") {
+      this.tracks = [];
+      return;
+    }
+    this.searchServ.getSearchResult(term).subscribe((data: any) => {
+      this.tracks = data.tracks.items;
+    }, (err) => {
+      console.error(err.message);
+    });
+  }
+
+  getPostsBySong(track: Track) {
+    this.tracks=[];
+    this.postServ.getPostsBySong(track).subscribe(
+      resp => {
+        this.searchPosts = resp;
+      }
+    )
+  }
 }
